bf3a459cba51f406dd752a92b6bbf18de7925ae7
Gabrielgs-strio <64772646+Gabrielgs-strio@users.noreply.github.com>
2020-09-03 16:11:50 +0200

    Update App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,7 +2,7 @@
     <v-app>
         <v-app-bar
                 app
-                color="#ff928f"
+                color="#8ffff6"
                 dark
                 shrink-on-scroll
                 prominent
